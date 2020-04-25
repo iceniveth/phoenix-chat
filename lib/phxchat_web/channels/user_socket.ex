cf767e9e5718b72ccb9654d5463d00438f8eee37
iceniveth <ken11zer01@gmail.com>
2020-04-25 21:18:17 +0800

    Add sample room chat

--- lib/phxchat_web/channels/user_socket.ex
+++ lib/phxchat_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PhxchatWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PhxchatWeb.RoomChannel
+  channel "room:*", PhxchatWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
